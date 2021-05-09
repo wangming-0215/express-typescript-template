eeda8b2a9b3d0bfb4ecb6deedb35d7cfbd4facc7
汪明 <1850889632@qq.com>
2021-05-09 13:26:52 +0800

    feat: configuration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
 import http from 'http';
 import express from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', (req, res) => {
+  res.status(200).json({ status: 'ok', data: 'Hello World' });
+});
+
 const server = http.createServer(app);
 
-console.log('hello world');
+const port = parseInt(process.env.PORT || '3000', 10);
+const host = process.env.HOST || 'localhost';
 
-server.listen(3000, () => console.log('connected'));
+server.listen(port, host, () => {
+  console.log(`Server started: http://${host}:${port}`);
+});
+
+// 19款 911 卡拉雷s
